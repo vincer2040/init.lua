0a499d508feeb9a406a5fe0076f2b6c0a416e843
boreddad <ronnebaumfinances@gmail.com>
2024-01-02 12:57:04 -0500

    highlight move remap

--- lua/vince/remap.lua
+++ lua/vince/remap.lua
@@ -11,6 +11,8 @@ vim.keymap.set("i", "jj", "<Esc>")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>y", "\"+y")
 
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { silent = true, noremap = true })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { silent = true, noremap = true })
